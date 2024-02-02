d092f6350f9a2ae3629871638e07e0c4a059ef32
Diego Fernando Mora Velasco <difemove@gmail.com>
2024-02-02 10:58:16 -0500

    change binaryFileHandler

--- libs/Movie.h
+++ libs/Movie.h
@@ -3,9 +3,33 @@
 
 #include <string>
 #include <sstream>
-#include "LineList.h"
+#include "List.h"
+#include "TextFileHandler.h"
 
 using namespace std;
+
+struct MovieBin {
+    int id;
+    int year;
+    int rating;
+    char title[100];
+
+    MovieBin() {
+        id = 0;
+        year = 0;
+        rating = 0;
+        strcpy(title, "");
+    }
+
+    bool equals(MovieBin other) {
+        return id == other.id
+                && year == other.year
+                && rating == other.rating
+                && strcmp(title, other.title) == 0;
+
+    }
+
+};
 
 struct Movie{
     int id;
@@ -27,8 +51,51 @@ struct Movie{
             && title == other.title;
 
     }
+
 };
 
+
+struct MovieBin2 {
+    
+    char title[100];
+    int id;
+    int year;
+    int rating;
+    
+
+    MovieBin2() {
+        id = 0;
+        year = 0;
+        rating = 0;
+        strcpy(title, "");
+    }
+
+    bool equals(MovieBin2 other) {
+        return id == other.id
+                && year == other.year
+                && rating == other.rating
+                && strcmp(title, other.title) == 0;
+
+    }
+};
+
+Movie toMovie(MovieBin movieBin) {
+    Movie movie;
+    movie.id = movieBin.id;
+    movie.year = movieBin.year;
+    movie.rating = movieBin.rating;
+    movie.title = movieBin.title;
+    return movie;
+}
+
+MovieBin toMovieBin(Movie movie) {
+    MovieBin movieBin;
+    movieBin.id = movie.id;
+    movieBin.year = movie.year;
+    movieBin.rating = movie.rating;
+    strcpy(movieBin.title, movie.title.c_str());
+    return movieBin;
+}
 Movie getMovieFromLine (string line, char delimiter) {
     
     // This is an alternative way to split a string
@@ -50,7 +117,7 @@ Movie getMovieFromLine (string line, char delimiter) {
 
     return aux_movie;*/
     
-    LineList words = split(line, delimiter);
+    List<string> words = split(line, delimiter);
 
     Movie aux_movie;
     aux_movie.id = stoi(words.get(0));
@@ -71,6 +138,65 @@ string getLineFromMovie(Movie movie, char delimiter) {
     line += movie.title;
 
     return line;
+}
+
+List<Movie> getMoviesFromLines(List<string> lines, char delimiter) {
+
+    List<Movie> movies;
+    for (int i = 0; i < lines.size; i++) {
+    
+        Movie movie = getMovieFromLine(lines.get(i), delimiter);
+        
+        movies.add(movie);
+    }
+
+    return movies;
+}
+
+List<string> getLinesFromMovies(List<Movie> movies, char delimiter) {
+    List<string> lines;
+    for (int i = 0; i < movies.size; i++) {
+        lines.add(getLineFromMovie(movies.get(i), delimiter));
+    }
+    return lines;
+}
+
+List<MovieBin> getMoviesBinFromMovies(List<Movie> movies) {
+    List<MovieBin> moviesBin;
+    for (int i = 0; i < movies.size; i++) {
+        MovieBin movieBin = toMovieBin(movies.get(i));
+        moviesBin.add(movieBin);
+    }
+    return moviesBin;
+}
+
+List<Movie> getMoviesFromMoviesBin(List<MovieBin> moviesBin) {
+    List<Movie> movies;
+    for (int i = 0; i < moviesBin.size; i++) {
+        Movie movie = toMovie(moviesBin.get(i));
+        movies.add(movie);
+    }
+    return movies;
+}
+
+float getAverageRating(List<Movie> movies) {
+    float sum = 0;
+    for (int i = 0; i < movies.size; i++) {
+        sum += movies.get(i).rating;
+    }
+    return sum / movies.size;
+}
+
+List<Movie> moviesWithratingGreaterThan(List<Movie> movies, double rating) {
+    List<Movie> newList;
+
+    for (int i = 0; i < movies.size; i++) {
+        if (movies.get(i).rating > rating) {
+            newList.add(movies.get(i));
+        }
+    }
+
+    return newList;
 }
 
 #endif /* MOVIE_H */
--- libs/TextFileHandler.h
+++ libs/TextFileHandler.h
@@ -1,7 +1,7 @@
-#ifndef FILEHANDLER_H
-#define FILEHANDLER_H
+#ifndef TEXT_FILEHANDLER_H
+#define TEXT_FILEHANDLER_H
 
-#include "LineList.h"
+#include "List.h"
 #include <string>
 #include <sstream>
 #include <fstream>
@@ -16,9 +16,9 @@ struct TextFileHandler {
         fileName = path;
     }
 
-    LineList readLines() {
+    List<string> readLines() {
 
-        LineList lines;
+        List<string> lines;
         
         fstream file;
         
@@ -39,7 +39,7 @@ struct TextFileHandler {
 
     }
 
-    bool writeLines(LineList lines, unsigned int mode = ios::out) {
+    bool writeLines(List<string> lines, unsigned int mode = ios::out) {
 
         fstream file;
 
@@ -84,7 +84,33 @@ struct TextFileHandler {
 
 };
 
+List<string> split(string line, char delimiter) {
+        
+        List<string> tokenList;
+        //split line into tokens
+        //stringstream is a stream class to operate on strings.
+        stringstream ss(line);
 
+        //see that ss.good() is used to check if the stream is good
+        //it means that it checks if the stream has not reached the end of the line
+        while (ss.good()) {
+            //declare a string variable to store the token
+            //a token is a string separated by a delimiter
+            //i.e 1;1994;90;The Shawshank Redemption
+            //  the delimiter is the semicolon (;)
+            //  the tokens are: 1, 1994, 90, The Shawshank Redemption
+            string token;
+            //getline() is used to read a string from the stream
+            //getline() extracts characters from the stream until it finds the delimiter character
+            //and stores them in the string object 'token'
+            getline(ss, token, delimiter);
 
+            //add token to the tokenList
+            tokenList.add(token);
+        }
+
+        return tokenList;
+
+}
 
 #endif /* MYHEADER_H */
